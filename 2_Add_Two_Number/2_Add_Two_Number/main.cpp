f6d71e297aa3fbbe036c914f97133832f351b899
codename <daction-z@hotmail.com>
2015-12-07 15:05:46 -0800

    2nd

---
+++ 2_Add_Two_Number/2_Add_Two_Number/main.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  2_Add_Two_Number
+//
+//  Created by David Zhong on 2015-12-07.
+//  Copyright © 2015 David Zhong. All rights reserved.
+//
+
+//You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+//
+//Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//Output: 7 -> 0 -> 8
+//
+//Subscribe to see which companies asked this question
+//
+//Hide Tags Linked List Math
+//Show Similar Problems
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+         int val;
+         ListNode *next;
+         ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    
+    return 0;
+
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
