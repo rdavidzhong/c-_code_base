ed3e894dc6a76475bc0024cd3271789595fcb77a
codename <daction-z@hotmail.com>
2015-12-09 12:52:57 -0800

    update add binary

--- 67_Add_Binary/67_Add_Binary/main.cpp
+++ 67_Add_Binary/67_Add_Binary/main.cpp
@@ -22,22 +22,56 @@
 //Show Similar Problems
 
 #include <iostream>
+#include <vector>
 #include <string>
 
 using namespace std;
 
 string addBinary(string a, string b) {
     
+    string result;
+    /*
+     
+     if (a.size() > b.size())
+     {
+        maxLength = a.begin();
+     }
+     else {
+        maxLength = b.begin();
+     
+     }
+     
+     */
     
+    int maxLength = a.size() > b.size()? a.size(): b.size();
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    int carry = 0;
+    
+    for(int i =0; i<maxLength;i++)
+    {
+            int ai = i<a.size()? a[i]-'0':0;
+            int bi = i<b.size()?b[i]-'0':0;
+            int val = (ai+bi+carry)%2;
+            carry = (ai+bi+carry)/2;
+            result.insert(result.begin(), val+'0');
+    }
     
     if (carry == 1)
     {
-        return.insert(result.begin(),val+'0');
+        result.insert(result.begin(),'1');
     }
-    return 0;
+    return result;
 }
 
 int main(int argc, const char * argv[]) {
+    
+    string a("11");
+    string b("1");
+    string result = addBinary(a, b);
+    
+    cout << result << endl;
+    
 
     return 0;
 }
