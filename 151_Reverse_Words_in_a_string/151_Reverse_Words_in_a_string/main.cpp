3713c6a2339602dea9b05f09361c8c7b75fe2548
codename <daction-z@hotmail.com>
2015-12-10 15:32:12 -0800

    Sync

---
+++ 151_Reverse_Words_in_a_string/151_Reverse_Words_in_a_string/main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  151_Reverse_Words_in_a_string
+//
+//  Created by David Zhong on 2015-12-10.
+//  Copyright © 2015 David Zhong. All rights reserved.
+//
+
+/*
+ Given an input string, reverse the string word by word.
+ 
+ For example,
+ Given s = "the sky is blue",
+ return "blue is sky the".
+ 
+ Update (2015-02-12):
+ For C programmers: Try to solve it in-place in O(1) space.
+ 
+ Hide Tags String
+ Show Similar Problems
+
+ */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void reverseWords(string &s) {
+ 
+}
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
