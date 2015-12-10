6747b85f6b5e90ef0b5143e01e260b51c601b438
codename <daction-z@hotmail.com>
2015-12-10 15:44:37 -0800

    Sync

---
+++ pointer/pointer/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  pointer
+//
+//  Created by David Zhong on 2015-12-10.
+//  Copyright © 2015 David Zhong. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
