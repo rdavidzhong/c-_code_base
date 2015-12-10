d39e66e0775ff8bcd64298e4a56412109cc9fab3
codename <daction-z@hotmail.com>
2015-12-10 12:21:19 -0800

    Sync

---
+++ 206_Reverse_Linked_List/206_Reverse_Linked_List/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  206_Reverse_Linked_List
+//
+//  Created by David Zhong on 2015-12-10.
+//  Copyright © 2015 David Zhong. All rights reserved.
+//
+
+/*
+ Reverse a singly linked list.
+ 
+ click to show more hints.
+ 
+ Hint:
+ A linked list can be reversed either iteratively or recursively. Could you implement both?
+ 
+
+ Hide Tags Linked List
+ Show Similar Problems
+
+ */
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
