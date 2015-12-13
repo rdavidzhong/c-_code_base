d277c4518e4e45b9e9ab7264052af7b1f905abc4
codename <daction-z@hotmail.com>
2015-12-13 15:16:43 -0800

    Sync

---
+++ Two-dimensional array/Two-dimensional array/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  Two-dimensional array
+//
+//  Created by David Zhong on 2015-12-13.
+//  Copyright © 2015 David Zhong. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    int dArray[10][10] = {{1,2},{3,4}};
+    
+    cout << dArray[0][0] << endl;
+    cout << dArray[0][1] << endl;
+    
+    cout << dArray[1][0] << endl;
+    cout << dArray[1][1] << endl;
+    
+    
+    return 0;
+}
