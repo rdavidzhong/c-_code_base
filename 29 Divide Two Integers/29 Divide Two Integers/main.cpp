abd30bf7ff17a8eba4e70d3b2d012e5d5eb825ea
codename <daction-z@hotmail.com>
2015-12-18 23:36:43 -0800

    Sync

--- 29 Divide Two Integers/29 Divide Two Integers/main.cpp
+++ 29 Divide Two Integers/29 Divide Two Integers/main.cpp
@@ -29,7 +29,32 @@ int divide(int dividend, int divisor) {
         sign = -1;
     }
     
+    unsigned int dvd = dividend > 0 ? dividend: -dividend;
+    unsigned int dvs = divisor > 0 ? divisor: -divisor;
+    unsigned int inc[32];
+    unsigned int migValue = dvs;
+    int i = 0;
     
+    while (migValue > 0 && migValue <= dvd)
+    {
+        inc[i] =  migValue;
+        migValue = migValue << 1;
+        i++;
+    }
+    i--;
+    
+    unsigned int res = 0;
+    while(i >= 0 && dvd != 0)
+    {
+        if(dvd >= inc[i])
+        {
+            dvd = dvd - inc[i];
+            res += 1<<i;
+        }
+        i--;
+    }
+    res *= sign;
+    return res;
         
 }
 
