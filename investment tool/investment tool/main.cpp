6cb25c0b27f3666cc98812a78a852f6ee2d856de
codename <daction-z@hotmail.com>
2015-12-29 21:50:26 -0800

    Sync

--- investment tool/investment tool/main.cpp
+++ investment tool/investment tool/main.cpp
@@ -12,43 +12,35 @@
 
 using namespace std;
 
-//int intPercent(int Percent, int numDisplay)
-//{
-//    Percent = 0.01;
-//    return Percent/numDisplay;
-//}
 
 int main(int argc, const char * argv[]) {
     
-    double price;
-    double myprice;
-    int shares;
+    double price = 35.5;
+    double myprice = 35.876;
+    int shares = 5900;
     double cal;
-    int numDisplay = 10; // number of result display - Default is 10
-   // int Percent = 0.01; // 1% = price*1.01
-  //  int intPercent;// Increase percentage each time
+    int numDisplay = 10;
+ 
     
     cout << "What's the price today?" << endl;
-    cin >> price;
+//    cin >> price;
     
-    cout << "What's your avg price?" << endl;
-    cin >> myprice;
+    cout << "What's your avg cost?" << endl;
+//    cin >> myprice;
     
     cout << "How many shares?" <<endl;
-    cin >> shares;
+//    cin >> shares;
     
     cout << endl;
     cout << "======Increment====== " << endl << endl;
     
-    //intPercent = Percent/numDisplay;
+  
     
     for (int i = 1 ; i <= numDisplay; i++)
     {
         // Every time increment 0.2%
         cal = 1+0.002*i;
         cout << i*0.2 << "% increase"<< endl;
-//        cal = 1+intPercent*i;
-//        cout << i*0.2 << "% increase"<< endl;
         cout << setprecision(4) << price*cal<< endl;
         cout << "Gain per share: " << setprecision(4) << price*cal-myprice << endl;
         cout << "Total gain: " << setprecision(5) << (price*cal-myprice)*shares << endl << endl;
@@ -56,22 +48,44 @@ int main(int argc, const char * argv[]) {
 
     cout << endl;
     
+    
+    cout << "======To Lower down Cost====== " << endl << endl;
+    
+    int more_share = 2700;
+    double lowerprice = 35.60;
+    double cost;
+    double newPrice;
+    
+    cout << "How many more can you buy?" << endl;
+//    cin >> more_share;
+    cout << "At what price?" << endl;
+//    cin >> lowerprice;
+  
+    cost = (myprice * shares + lowerprice * more_share)/(shares+more_share);
+    
+    cout << "New Avg cost: " << setprecision(4) << cost << endl << endl;
+    
+    cout << "How much per share right now? " << endl;
+    cin >> newPrice;
+    
+    cal =  newPrice - cost;
+    
+    if (cal > 0)
+    {
+        cout << "Gain: " << cal << " /share" << endl;
+        cout << "Total: " << cal*(shares+more_share) << endl;
+    }
+    else
+    {
+        cout << "Loss: " << cal << " /share" << endl;
+        cout << "Total: " << cal*(shares+more_share) << endl;
+    }
+    cout << endl;
+    
+    
     return 0;
 }
 
-
-//    cout << "1% = " << setprecision(4) << price*1.01 << endl;
-//    cout << "Gain per share: " << setprecision(4) << price*1.01-myprice << endl;
-//    cout << "Total gain: " << setprecision(5) << (price*1.01-myprice)*shares << endl << endl;
-//
-//    cout << "1.2% = "  << price*1.012 <<endl;
-//    cout << "Gain per share: " << setprecision(4) << price*1.012-myprice << endl;
-//    cout << "Total gain: " << setprecision(5) << (price*1.012-myprice)*shares << endl << endl;
-//
-//    cout << "1.5% = " << price*1.015 <<endl;
-//    cout << "1.8% = " << price*1.018 <<endl;
-//    cout << "2% = " << price*1.02 <<endl;
-//
 //    cout << endl;
 //    cout << "Decrement " << endl;
 //    cout << "1% = " << price*0.99 <<endl;
